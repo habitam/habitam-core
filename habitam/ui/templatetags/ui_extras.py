0c9c354130120f85954e8e1426779887df07db5b
Ștefan Gună <svguna@gmail.com>
2013-05-26 18:19:30 +0300

    refactored operation list to use custom templates, added correct sign for collections

--- habitam/ui/templatetags/ui_extras.py
+++ habitam/ui/templatetags/ui_extras.py
@@ -24,24 +24,32 @@ from habitam.entities.models import ApartmentGroup
 
 register = template.Library()
 
-@register.filter(name='operation_sign')
-def operation_sign(val, negate):
-    if val == None:
-        val = 0
-    if negate:
-        return val * -1
-    return val
+@register.assignment_tag
+def operation_amount(account, doc, service):
+    if doc == None or doc.total_amount == None:
+        return 0
+    if doc.src == account and not doc.type == 'collection':
+        return doc.total_amount * -1
+    return doc.total_amount
 
+@register.assignment_tag
+def operation_other_party(account, doc):
+    if doc.src == account:
+        return doc.billed.holder
+    return doc.src.holder
 
 @register.assignment_tag
 def available_buildings():
     return ApartmentGroup.objects.filter(type='building')
 
 @register.simple_tag
-def op_amount_class(doc, negative, amount, service):
+def op_amount_class(account, doc, service):
     if doc.type == 'collection' and service != None:
-        return 'collection'
-    if negative and amount > 0 or not negative and amount < 0:
+        return ''
+    amount = doc.total_amount
+    if doc.src == account:
+        amount = amount * -1
+    if amount < 0:
         return 'negative_balance'
     return 'positive_balance'
 
