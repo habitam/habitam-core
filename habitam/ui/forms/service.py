e1f69e0b125bfca8909de0632d142d28cb483657
Ștefan Gună <svguna@gmail.com>
2013-06-03 22:53:58 +0300

    simplified new invoice form

--- habitam/ui/forms/service.py
+++ habitam/ui/forms/service.py
@@ -106,12 +106,26 @@ class EditServiceForm(forms.ModelForm):
     
 
 class NewServiceInvoice(NewDocPaymentForm):
+    due_date = forms.DateField(label='Data scadenta', widget=SelectDateWidget,
+                        help_text="Data scadenta de plata a facturii.")
+    start_service_date = forms.DateField(label='Data inceput perioada serviciu',
+                        widget=SelectDateWidget, required=False, 
+                        help_text="Inceputul perioadei furnizarii serviciului facturat.")
+    end_service_date = forms.DateField(label='Data sfarsit perioada serviciu', 
+                        widget=SelectDateWidget, required=False,
+                        help_text="Sfarsitul perioadei furnizarii serviciului facturat.")
+    document_id = forms.CharField(label='ID factura', required=False,
+                        help_text="Id-ul facturii folosit la tranzactile electronice")
+    quantity = forms.DecimalField(label='Cantitatea', initial=1, required=False,
+                        help_text="Cantitatea facturata")
+    unit_type = forms.ChoiceField(label='Unitatea de masura',
+                        choices=OperationDoc.UNIT_TYPES, required=False,
+                        help_text="Unitatea de masura")
+    vat = forms.DecimalField(label='TVA', initial=24, help_text="Cota TVA")
    
     def __init__(self, *args, **kwargs):
         self.service = kwargs['entity']
         super(NewServiceInvoice, self).__init__(*args, **kwargs)
-        
-        self.__add_fiscal_details()
         
         if self.service.quota_type == 'noquota':
             self.fields['amount'] = forms.DecimalField(label='Suma',
@@ -128,20 +142,6 @@ class NewServiceInvoice(NewDocPaymentForm):
                 self.fields[var_name + str(ap.pk)] = DecimalField(
                             label=label + str(ap), decimal_places=3,
                             max_digits=4)
-                
-    def __add_fiscal_details(self):
-        
-        self.fields['due_date'] = forms.DateField(label='Data scadenta', widget=SelectDateWidget, help_text="Data scadenta de plata a facturii.")
-        self.fields['start_service_date'] = forms.DateField(label='Data inceput perioada serviciu', widget=SelectDateWidget, required=False, help_text="Inceputul perioadei furnizarii serviciului facturat.")
-        self.fields['end_service_date'] = forms.DateField(label='Data sfarsit perioada serviciu', widget=SelectDateWidget, required=False, help_text="Sfarsitul perioadei furnizarii serviciului facturat.")
-        
-        self.fields['document_id'] = forms.CharField(label='ID factura', required=False, help_text="Id-ul facturii folosit la tranzactile electronice")
-        
-        self.fields['quantity'] = forms.DecimalField(label='Cantitatea', initial=1, required=False, help_text="Cantitatea facturata")
-        self.fields['unit_type'] = forms.ChoiceField(label='Unitatea de masura', choices=OperationDoc.UNIT_TYPES, required=False, help_text="Unitatea de masura")
-        
-        self.fields['vat'] = forms.DecimalField(label='TVA', initial=24, help_text="Cota TVA")
-        
         
     def clean_apartments(self, cleaned_data, label):
         all_data = {}
