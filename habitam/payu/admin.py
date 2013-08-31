ed206315e9a1cf50628777b9b97dd8a99173d3f0
Ștefan Gună <svguna@gmail.com>
2013-08-31 20:45:44 +0300

    added the online payment order amount to the admin model

--- habitam/payu/admin.py
+++ habitam/payu/admin.py
@@ -20,10 +20,16 @@ Created on Aug 31, 2013
 @author: Stefan Guna
 '''
 from django.contrib import admin
-from habitam.payu.models import Order
+from django.db.models.aggregates import Sum
+from habitam.payu.models import Order, ApartmentAmount
+
+def order_amount(order):
+    r = ApartmentAmount.objects.filter(order=order).aggregate(Sum('amount'))
+    return r['amount__sum']
+order_amount.short_description = 'Suma'
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('created', 'status', 'user')
+    list_display = ('created', 'status', 'user', order_amount)
 
 admin.site.register(Order, OrderAdmin)        
     
