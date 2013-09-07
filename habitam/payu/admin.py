7160638a636dcf203b39925a20be92e20c88d60e
Ștefan Gună <svguna@gmail.com>
2013-09-07 15:41:48 +0300

    added payu complete debug

--- habitam/payu/admin.py
+++ habitam/payu/admin.py
@@ -22,6 +22,7 @@ Created on Aug 31, 2013
 from django.contrib import admin
 from django.db.models.aggregates import Sum
 from habitam.payu.models import Order, ApartmentAmount, OrderComplete
+from habitam.payu.payu import complete_order
 
 def order_amount(order):
     r = ApartmentAmount.objects.filter(order=order).aggregate(Sum('amount'))
@@ -30,6 +31,21 @@ order_amount.short_description = 'Suma'
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('created', 'status', 'user', 'building', order_amount)
+    
+    actions = ['complete_payu_payment']
+    
+    def complete_payu_payment(self, request, queryset):
+        updated = 0
+        for l in queryset:
+            try:
+                complete_order(l.id)
+            except Exception as e:
+                self.message_user(request, 'Operation failed for %s because "%s"' % (str(l), str(e)))
+                return
+            updated += 1
+        self.message_user(request, 'Completed %d payments' % updated)
+            
+    complete_payu_payment.short_description = '[DEBUG] Complete PAYU payments'
     
 class OrderCompleteAdmin(admin.ModelAdmin):
     pass
