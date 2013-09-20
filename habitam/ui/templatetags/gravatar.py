09eafda673eea928e1531dca4c51eedf86b9a8da
Ștefan Gună <svguna@gmail.com>
2013-09-20 22:30:46 +0300

    updated gravatar url

--- habitam/ui/templatetags/gravatar.py
+++ habitam/ui/templatetags/gravatar.py
@@ -43,7 +43,7 @@ class GravatarUrlNode(template.Node):
         default = current_site.domain + static('ui/img/habitam-worker-user.png')
         size = 80
  
-        gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
+        gravatar_url = "https://secure.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
         gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
  
         return gravatar_url
