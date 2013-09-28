c386e5761efb2562bb664d61d5dac158d9106df4
Ștefan Gună <svguna@gmail.com>
2013-09-28 11:55:44 +0300

    pulled translation strings

--- habitam/urls.py
+++ habitam/urls.py
@@ -35,4 +35,5 @@ urlpatterns = patterns('',
     url(r'^payu/', include('habitam.payu.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^captcha/', include('captcha.urls')),
+    url(r'^lang/', include('django.conf.urls.i18n')),
 )
